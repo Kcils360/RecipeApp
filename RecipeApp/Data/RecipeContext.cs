a07d81320800b9d9c790ad668114f668aeb6fc3f
GREGORY DUKES <DUKES.GREGORYD@GMAIL.COM>
2017-12-31 13:53:03 -0800

    created recipe model, scaffolded recipe controller with views, initail migration and db update

---
+++ RecipeApp/Data/RecipeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeApp.Models
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext (DbContextOptions<RecipeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RecipeApp.Models.Recipe> Recipe { get; set; }
+    }
+}
