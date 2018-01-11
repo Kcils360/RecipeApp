d8908dd18dd70ff0925191de02a0f05ccfbf33be
GREGORY DUKES <DUKES.GREGORYD@GMAIL.COM>
2018-01-11 15:33:52 -0800

    still working on displaying ingredes list

--- RecipeApp/Models/Recipe.cs
+++ RecipeApp/Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace RecipeApp.Models
     public class Recipe
     {
         public int ID { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Ingredients { get; set; }
+
+        [Required]
         public string Directions { get; set; }
 
     }
